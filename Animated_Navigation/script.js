37355c1aca9bb1fbf598f6b62646124334d159ce
Bhanu Vikram <5193270+BhanuVikram@users.noreply.github.com>
2024-06-08 08:19:33 -0700

    Project: Animated Navigation

---
+++ Animated_Navigation/script.js
@@ -0,0 +1,6 @@
+const toggler = document.querySelector(".toggler");
+const nav = document.querySelector("nav");
+
+toggler.addEventListener("click", () => {
+  nav.classList.toggle("collapsed");
+});
